a013afb0e255d40f5a4aebba77a1c38e4d67efcc
lavollmer <141365306+lavollmer@users.noreply.github.com>
2023-11-05 12:10:41 -0600

    troubleshoot connection to workbench

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection(
   {
     host: 'localhost',
     // env variables
-    user: process.env.USER,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
   },
--- server.js
+++ server.js
@@ -9,54 +9,50 @@ const inquirer = require('inquirer');
 const table = require('table');
 
 //import business db
-const db = require('./config/connection.js');
+const db = require('./config/connection');
 
 // inquirer prompt to ask questions
-inquirer.prompt([
-  {
-    //THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-    type: 'list',
-    message: 'What would you like to do',
-    name: 'mainMenu',
-    choices: ["All departments", "All Employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
-  }]).then(answers => {
-    //case and switch statement with mainMenu choices
-    switch (answers.mainMenu) {
-      case 'All departments':
-        db.execute('SELECT * FROM department', function (err, results) {
-          if (err) {
-            //err message
-            console.log("This is an error");
-            return;
-          } else {
-            //table the results
-            console.log(table(results));
-            return;
-          }
-        });
-        break;
-      case 'All Employees':
+// inquirer.prompt([
+//   {
+//     //THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+//     type: 'list',
+//     message: 'What would you like to do',
+//     name: 'mainMenu',
+//     choices: ["All departments", "All Employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+//   }]).then(answers => {
+//     //case and switch statement with mainMenu choices
+//     switch (answers.mainMenu) {
+//       case 'All departments':
+//         db.query('SELECT * FROM department', function (err, results) {
+//           if (err) {
+//             //err message
+//             console.log("This is an error");
+//             return;
+//           } else {
+//             //table the results
+//             console.log(table(results));
+//             return;
+//           }
+//         });
+//         break;
+//       case 'All Employees':
 
-        break;
-      case 'Add a department':
+//         break;
+//       case 'Add a department':
 
-        break;
-      case 'Add a role':
+//         break;
+//       case 'Add a role':
 
-        break;
-      case 'Add an employee':
+//         break;
+//       case 'Add an employee':
 
-        break;
-      case 'Update an employee role':
+//         break;
+//       case 'Update an employee role':
 
-        break;
-      default:
-    }
-  });
+//         break;
+//       default:
+//     }
+//   });
 
 
-
-// Query database
-// db.query('SELECT * FROM students', function (err, results) {
-//   console.log(results);
-// });
+db.query('SELECT * FROM department', (err, data) => err ? console.log(err) : console.log(data))
